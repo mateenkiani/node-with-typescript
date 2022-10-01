96206a81c7442be8c0af088e0daeb3ba00955e06
Mateen Kiani <kiani.mateen012@gmail.com>
2022-10-01 08:05:55 +0500

    added dev script to watch for changes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World 4!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
